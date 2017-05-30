796e92281bf4d77a55ae219dce392419d7df85fb
ALEKSANDER ONISZCZAK <aleks@aleks-macbook-pro.local>
2017-05-30 12:05:25 -0400

    sdferthtyjkul

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonClicked(_ sender: UIButton) {
-        print("you finished! one more change")
+        print("you finished! one more change zdsvdfgfncghmjgfhgsfsfsfgs")
     }
     
     @IBAction func butt2(_ sender: UIButton) {
